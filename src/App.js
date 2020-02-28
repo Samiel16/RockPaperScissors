a93ea13af7d9dc8af2df5a52315bb00a12d8414d
unknown <samielperez16@gmail.com>
2020-02-27 20:35:13 -0800

    Keep working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import PlayerCard from './PlayerCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class Game extends Component {
+  constructor() {
+    super();
+    this.signs = ["Rock", "Scissors", "Paper"];
+    this.state={
+      playerOne: "Rock",
+      playerTwo: "Paper",
+    }
+  }
 
-export default App;
+  //this is where my methods live
+
+  playGame = () => {
+    this.setState({
+      playerOne: this.signs[Math.floor(Math.random()*3)],
+      playerTwo: this.signs[Math.floor(Math.random()*3)],
+    })
+  }
+
+  decideWinner = () => {
+    const playerOne = this.state.playerOne;
+    const playerTwo = this.state.playerTwo;
+      if(playerOne === playerTwo) {
+        return "It's a tie";
+      }else if((playerOne === "Rock" && playerTwo === "Scissors") ||
+      (playerOne === "Scissors" && playerTwo === "Paper") ||
+      (playerOne === "Paper" && playerTwo === "Rock")){
+        return "Player 1 wins!";
+      } else{
+        return "Player 2 wins!"
+      }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <PlayerCard sign={this.state.playerOne}/>
+          <PlayerCard sign={this.state.playerTwo}/>
+        </div>
+        <div className="winner">This is the winner!</div>
+        <button type="button" onClick={this.playGame}>Play the Game</button>
+      </div>
+    )
+  }
+};
+
+export default Game;
